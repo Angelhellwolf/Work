9204c359ce7f19049f752f086beaf8dbd20b0717
AngelHell <angelhellwolf@gmail.com>
2023-09-04 17:16:23 +0800

    第二天

---
+++ src/Day02.java
@@ -0,0 +1,6 @@
+public class Day02 {
+    public static void main(String[] args) {
+        //int double char boolean byte short long float
+        //int 4字节 32位 double 8字节 64位 char 2字节 16位 boolean 1字节 8位 byte 1字节 8位 short 2字节 16位 long 8字节 64位 float 4字节 32位
+    }
+}
