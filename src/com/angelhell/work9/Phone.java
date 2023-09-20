08862d69f5968cd1d16c809b5714d942dfa7fb6b
angel <angelhellwolf@gmail.com>
2023-09-20 13:50:32 +0800

    我真是大聪明

---
+++ src/com/angelhell/work9/Phone.java
@@ -0,0 +1,37 @@
+package com.angelhell.work9;
+
+public class Phone {
+    private String brand;
+    private double price;
+    private String color;
+
+    public Phone(String brand, double price, String color) {
+        this.brand = brand;
+        this.price = price;
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
